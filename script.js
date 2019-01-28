f7a8e86b3357b814da4f1e9c0c7098fefc17ceba
simonashabanova <43668759+simonashabanova@users.noreply.github.com>
2019-01-28 12:24:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+//
+
+// var height = 1.83;
+// var limit = 1.63;
+
+// if(height > limit) {
+//     console.log("Mozes da se vozis na rolerkosterot.");
+// } else {
+//     console.log("Ne mozes da se vozis na rolerkosterot.");
+// }
+
+// var num1 = 5;
+// var num2 = 7;
+
+// if(num1 > num2) {
+//     console.log(num1);
+// } else if(num1 < num2) {
+//     console.log(num2);
+// } else {
+//     console.log("Numbers are equal");
+// }
+
+// var today = "Monday";
+
+// switch(today) {
+//     case "Monday":
+//         console.log("Today is Monday");
+//     break;
+//     case "Tuesday":
+//         console.log("Today is Tuesday")
+//     break;
+//     case "Wednesday" :
+//         console.log("Today is Wednesday")
+//     break;
+//     case "Thursday":
+//         console.log("Today is Thursday")
+//     break;
+//     case "Friday":
+//         console.log("Today is Friday")
+//     break;
+//     case "Saturday":
+//         console.log("Today is Saturday")
+//     break;
+//     case "Sunday":
+//         console.log("Today is Sunday")
+//     break;
+//     default: console.log("Unknow day");
+// }
+
+// function SayHello() {
+//     console.log("Hello World");
+// }
+
+// SayHello(); 
+
+// function SayHelloAgain(name) {
+//     return "Hello " + name;
+// }
+
+// console.log( SayHelloAgain("Mario") );
+
+// function MarioPyramid(size) {
+//     for(var i = 1; i <= size; i++ ){
+//         var result = "";
+
+//         for(var j = 1; j <= i; j++){
+//             result += "#";
+//         }
+
+//         console.log(result);
+//     }
+// }
+
+// MarioPyramid(5);
+
+
+// console.dir(document.querySelector("h2"));
+// document.querySelector("h2").innerHTML = "Hello World";
+
+// var button = document.querySelector( ".date-button");
+// var result = document.querySelector(".date");
+
+// button.addEventListener("dblclick", function() {
+//     result.innerHTML = new Date();
+// });
